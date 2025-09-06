d61c2e3f01454d82f0239f7a59d6dde6f0cf22a4
Camila Soares <camilagoulartsoares@yahoo.com>
2025-09-06 09:10:21 -0300

    electronAPI

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,6 @@ export default function App() {
     
     alarmAudioRefs.current = [];
  
-
-    
     for (let i = 0; i < 3; i++) {
       const audio = new Audio("/assets/alarm.wav");
       audio.loop = true;
