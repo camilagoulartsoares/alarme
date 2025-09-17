2e8d32f60bc9d5f52a7c5d7a38e7411edd5be309
Camila Soares <camilagoulartsoares@yahoo.com>
2025-09-17 08:14:43 -0300

    stopSoundOnly

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ export default function App() {
     timersRef.current = {};
   };
      
+
+  
   const stopSoundOnly = () => {
     alarmAudioRefs.current.forEach((audio) => {
       audio.pause();
