e6ec236e97eb3c1750154317c57767076c66cf03
Camila Soares <camila.soares@gfinnovation.com.br>
2025-10-06 15:29:30 -0300

    stopSoundOnly

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,28 @@ export default function App() {
     alarmAudioRefs.current = [];
   };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   const triggerAlarm = useCallback(() => {
     setIsRinging(true);
     setCanStop(false);
