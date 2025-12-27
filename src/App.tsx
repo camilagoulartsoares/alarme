3cd305c1a801536c60f2bd1620daddd3eed1941d
Camila Soares <camilagoulartsoares@yahoo.com>
2025-12-27 15:50:14 -0300

    triggerAlarm

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,20 @@ export default function App() {
     alarmAudioRefs.current = [];
   };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const triggerAlarm = useCallback(() => {
     setIsRinging(true);
     setCanStop(false);
