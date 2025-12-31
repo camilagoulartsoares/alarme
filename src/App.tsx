0c7d8b793f1f4480653f22dc0b7942ebfd077217
Camila Soares <camilagoulartsoares@yahoo.com>
2025-12-31 05:07:01 -0300

    clearTimers

--- src/App.tsx
+++ src/App.tsx
@@ -105,44 +105,6 @@ export default function App() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   const triggerAlarm = useCallback(() => {
     setIsRinging(true);
     setCanStop(false);
