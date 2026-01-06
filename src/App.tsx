96cfb6fa1582753465e46879e0a792d5d4119fe6
Camila Soares <camilagoulartsoares@yahoo.com>
2026-01-06 05:37:44 -0300

    triggerAlarm

--- src/App.tsx
+++ src/App.tsx
@@ -57,32 +57,6 @@ export default function App() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   const triggerAlarm = useCallback(() => {
     setIsRinging(true);
     setCanStop(false);
