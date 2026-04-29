f13befa51f31bce833200c90c9a24ee1ced37041
Camila Soares <camilagoulartsoares@yahoo.com>
2026-04-29 04:30:42 -0300

    waiting

--- src/App.tsx
+++ src/App.tsx
@@ -219,7 +219,6 @@ export default function App() {
           ) : (
             <p className="waiting">Espere 40 segundos para parar o alarme</p>
           )}
-          
         </div>
       )}
     </div>
