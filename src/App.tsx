25206e43c31bf36bde83b6347c8c3a2e8493d1aa
Camila Soares <camilagoulartsoares@yahoo.com>
2026-05-21 11:35:08 -0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ export default function App() {
               </button>
 
               <button className="close-app" onClick={closeApp}>
-                Fechar Despert
+                Fechar Despertador
               </button>
             </div>
           )}
